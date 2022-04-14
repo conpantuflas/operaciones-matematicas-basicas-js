a6df30a3d16ff73c19aa75ca139cbfdc1597c26e
conpantuflas <3280kg@gmail.com>
2022-04-14 04:35:31 +0000

    primer comit

---
+++ js.js
@@ -0,0 +1,29 @@
+//OPERACIONES CON FUNCIONES JS
+
+// resta
+function resta(a,b){
+    let resultado = a - b
+    return resultado
+}
+console.log(resta(13,6))
+
+//suma
+function suma(a,b){
+    let resultado = a + b
+    return resultado
+}
+console.log(suma(6,9))
+
+//division
+function division(a,b){
+    let resultado = a / b
+    return resultado
+}
+console.log(division(60,8))
+
+//nultiplicacion
+function multiplicacion(a,b){
+    let resultado = a * b
+    return resultado
+}
+console.log(multiplicacion(5,2))
